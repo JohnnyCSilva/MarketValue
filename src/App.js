2a03dc3663fe39fac14402f6fcc39634078bac71
João Silva <joaocsilva666@gmail.com>
2022-09-23 21:00:40 +0100

    SideBar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import React from'react';
+import NavBarDesktop from './Components/NavBar/NavBarDesktop';
+import NavBarMobile from './Components/NavBar/NavBarMobile';
+import 'primeicons/primeicons.css';
 
 function App() {
   return (
-    <div>
+    <div className='App'>
+      <div className='App__Body'>
+
+        <div className='App__Header'>
+          <NavBarDesktop />
+          <NavBarMobile />
+        </div>
+        <div className="App__Content">
+
+        </div>
+      </div>
     </div>
   );
 }
