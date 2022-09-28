b5dc7943cded308e76ff4c5ff6acb5bb33f1b180
Alexandre Neves <alexandre.neves@garland.pt>
2022-09-28 09:22:22 +0100

    🔑

--- config/Firebase.js
+++ config/Firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHhoo7K8FeTgn09LIOdvRnesJWG23zKLM",
-  authDomain: "marketvalue-1.firebaseapp.com",
-  projectId: "marketvalue-1",
-  storageBucket: "marketvalue-1.appspot.com",
-  messagingSenderId: "299043931076",
-  appId: "1:299043931076:web:3b23c063a900a431475a91"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
