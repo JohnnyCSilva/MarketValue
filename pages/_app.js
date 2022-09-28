c206bbcbcf8592b01264d27e43c8654b86513c07
Alexandre Neves <alexandre.neves@garland.pt>
2022-09-28 22:32:03 +0100

    🔥 Basic Firebase Authentication

--- pages/_app.js
+++ pages/_app.js
@@ -8,11 +8,15 @@ import '../config/Firebase.js'
 import 'primeicons/primeicons.css';
 import "primereact/resources/primereact.css";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
-
+import { AuthProvider } from '../config/AuthContext';
 
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
 
 export default MyApp
