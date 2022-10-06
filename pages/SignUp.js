9f2afcf95c0023e7c0223cccf53f1f47ac15082a
João <joaocsilva666@gmail.com>
2022-10-06 16:12:06 +0100

    New SignIn missing Background

--- pages/SignUp.js
+++ pages/SignUp.js
@@ -5,50 +5,65 @@ const SignUp = () => {
 
   const {user, logIn, logOut} = useAuth();
 
-  const handleRightSide = () => {
-    var form = document.getElementById('form_Container')
-    form.style.display = 'flex';
+  const showPassword = () => {
+    var password = document.getElementById('password');
+    var icon = document.getElementById('show__password');
+
+    if (password.type === "password") {
+      icon.classList.remove('pi-lock');
+      icon.classList.add('pi-unlock');
+      password.type = "text";
+    } else {
+      icon.classList.remove('pi-unlock');
+      icon.classList.add('pi-lock');
+      password.type = "password";
+    }
+
   }
 
   return (
-    <div className="SignUp__content">
-        <div className="SignUp__container">
-            <div className="SignUp__confirm">
-                <img src="/images/img_1.svg"  alt="" />
-                <h1>A safe place for you to control your finance</h1>
-                <p>By continuing the create account setup,
-                you agree to our terms & 
-                conditions and you also agree that we 
-                can send you personalized newsletters.</p>
-                <div className="confirm__arrow">
-                    <a href="/">Back</a>
-                    <button onClick={handleRightSide} type="button"><i className="pi pi-arrow-right"></i></button>
-                </div>
-            </div>
-            <div className="form__Container" id="form_Container">
-            <div className="SignUp__form">
-                <img src="/images/img_2.svg" alt="" />
-                <h1>Let's get Stated</h1>
-                <form className="form__SignUp">
-                  <div className="input_field">
-                    <i className="pi pi-at"></i>
-                    <input type="text" placeholder="Email Address" />
-                  </div>
-                  <div className="input_field">
-                    <i className="pi pi-lock"></i>
-                    <input type="password" placeholder="Password" />
-                  </div>
-                  <button type="submit" >Create Account </button>
-                </form>
-
-
-                <div className="google_SignUp" onClick={logIn}>
-                  <img src="/images/google_logo.png" alt="" />
-                  <p>Sign Up with Google</p>
-                </div>
-            </div>
-            </div>
+    <div className="main__signUp">
+      <div className="container__signUn__left">
+        <div className="signUp__form__total">
+          <div className="signUp__Title">
+            <h1>Sign Up </h1>
+            <p>See your growth and get consulting support!</p>
+          </div>
+          <div className='signUp__buttons'>
+            <a href="/" onClick={logIn}>
+              <img src="/images/google_logo.png" alt="GoogleAuthProvider" />
+              <p> Sign up with Google</p>
+            </a>
+          </div>
+          <div class="separator">
+            <span>or Sign in with Email</span>
+          </div>
+          <div className='signUp__form'>
+            <form>
+              <label>Username</label>
+              <div className='signUp__input'>
+                <i className="pi pi-user" />
+                <input type="text" name="Name" placeholder="Name" required/>
+              </div>
+              <label>Email</label>
+              <div className='signUp__input'>
+                <i className="pi pi-at" />
+                <input type="email" name="Email" placeholder="Email" required/>
+              </div>
+              <label>Password</label>
+              <div className='signUp__input'>
+                <i className="pi pi-lock" id="show__password" onClick={showPassword}/>
+                <input type="password" name="Password" id="password" placeholder="Password" required/>
+              </div>
+              <button type="submit" className="btn__submit">Create Account</button>
+            </form>
+          </div>
+          <div className="signUp__after">
+            <p> Already have an account? <a href="/">Sign In</a></p>
+          </div>
         </div>
+      </div>
+
     </div>
   )
 }
