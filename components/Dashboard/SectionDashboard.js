f6745990c1fe74725b5ec9a427e0ee65e84b20d6
João Silva <joaocsilva666@gmail.com>
2022-10-17 22:46:28 +0100

    coisas

--- components/Dashboard/SectionDashboard.js
+++ components/Dashboard/SectionDashboard.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { LineChart, Line, ResponsiveContainer, Tooltip, YAxis } from 'recharts';
 import moment from 'moment';
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { collection, query, where, getDocs, addDoc, serverTimestamp } from "firebase/firestore";
 import { db } from '../../config/Firebase'
 import { useAuthValue } from '../../config/AuthContext'
 
@@ -10,54 +10,81 @@ const SectionDashboard = () => {
 
     const { currentUser } = useAuthValue();
     let totalPrice = 0;
+    let currentPrice = 0;
     const [portfolioValue, setPortfolioValue] = useState();
-   
+    const [sparkline, setSparkline] = useState([]);
+    const [total, setTotal] = useState();
+    const [value, setValue] = useState(null);
+    const [date, setDate] = useState(null);
+    const [interval, setInterval] = useState("daily");    
+
     useEffect(() => {
         if (currentUser) {
             getCoinsFromUser();
         }
-        },[currentUser])
+    },[currentUser])
    
     const getCoinsFromUser = async() =>{
         const queryDB = query(collection(db, "coins"), where("userId", "==", currentUser.uid));
         const getCoinsFromUserDB = await getDocs(queryDB);
         getCoinsFromUserDB.forEach((doc) => {
-            //console.log(doc.id, " => ", doc.data());
-            totalPrice += doc.data().current_price;
-        });   
-       console.log((totalPrice).toFixed(2), "€");
-       setPortfolioValue((totalPrice).toFixed(2));
+            var currentCoin = doc.data().id;
+            axios.get(`https://api.coingecko.com/api/v3/coins/${currentCoin}?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false`).then(res => {
+                totalPrice += res.data.market_data.current_price.eur;
+                setPortfolioValue((totalPrice).toFixed(2));  
+                /*addDoc(collection(db, "makeGraph"), {
+                    userId: currentUser.uid, 
+                    id: res.data.id,
+                    symbol: res.data.symbol,
+                    name: res.data.name,
+                    current_price: res.data.market_data.current_price.eur,
+                    timestamp: serverTimestamp(),
+                });      */
+            })  
+        }); 
+        
     }
 
-    const [sparkline, setSparkline] = useState([]);
-    const [portValue, setPortValue] = useState();
-    const [value, setValue] = useState(null);
-    const [date, setDate] = useState(null);
-    const [interval, setInterval] = useState("daily");    
+    useEffect((currentUser, totalPrice) => {
+        if (currentUser, totalPrice) {
+            getCoinsToMakeGraph();
+        }
+    },[currentUser,totalPrice])
+    // NOT WORKING 
+
+    const getCoinsToMakeGraph = async() => {
+        console.log("teste");
+        const queryGraph = query(collection(db, "makeGraph"), where("userId", "==", currentUser.uid));
+        const makeGraphWithTimeStamp = await getDocs(queryGraph);
+        makeGraphWithTimeStamp.forEach((doc) => {
+            console.log("teste")
+            var currentTimeStamp = doc.data().timeStamp;
+            // faz array de tempo e coloca no X
+            currentPrice += doc.data().current_price;
+            // faz array de tempo e coloca no Y
+            setSparkline({ x: moment(currentTimeStamp).format('MMMM Do YYYY, h:mm:ss a'), y: (currentPrice).toFixed(2) });
+            console.log(sparkline); 
+        });
+    }
 
     const CustomTooltip = ({ active, payload }) => {
         if (active && payload && payload.length) {
-            setPortValue(payload[0].value);
+            setTotal(payload[0].value);
             setDate(payload[0].payload.x)
         }
     };
 
-    // Le o ID da moeda e vai buscar os dados da moeda ao coingecko
-    // apresenta as moedas que ele tem em baixo e junta todos os seus valores atuais
-    // guarda os dados delas com o seu timestamp para apresentar um gráfico que funcione através de tempo
-
-    useEffect(() => {
+    /*useEffect(() => {
         if (value) {
-            console.log(value);
             axios.get(`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=eur&days=${value}&interval=${interval}`).then(res => {
-            setSparkline(res.data.prices.map(value => ({ x: moment(value[0]).format('MMMM Do YYYY, h:mm:ss a'), y: (value[1].toFixed(2)) })));
+            setSparkline(res.data.prices.map(value => ({ x: moment(value[0]).format('MMMM Do YYYY, h:mm:ss a'), y: (value[1].toFixed(2))})));
             }).catch(error => console.log(error));
         } else {
             axios.get(`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=eur&days=24&interval=daily`).then(res => {
                 setSparkline(res.data.prices.map(value => ({ x: moment(value[0]).format('MMMM Do YYYY'), y: (value[1].toFixed(2)) })));
             }).catch(error => console.log(error));
         }
-    }, [value]);
+    }, [value]);*/
 
 
   return (
@@ -69,7 +96,7 @@ const SectionDashboard = () => {
             <div className="dashboard__graph__header">
                 <div className="dashboard__graph__left">
                     <p>{date}</p>
-                    <h1>{portValue} total: {portfolioValue}<span> €</span></h1>
+                    <h1>{total} total: {portfolioValue}<span> €</span></h1>
                 </div>
                 <div className='dashboard__graph__right'>
                     <div className="graph__options">
